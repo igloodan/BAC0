c0cf79ceb0b82d2c0486439f92f6c54d8f79c481
Daniel McMahon <daniel@igloocontrols.com>
2021-07-29 15:59:22 +0100

    logging changes 1559

--- BAC0/core/utils/notes.py
+++ BAC0/core/utils/notes.py
@@ -181,48 +181,48 @@ def note_and_log(cls):
     cls._notes.notes = []
 
     # Defining log object
-    cls.logname = "{} | {}".format(cls.__module__, cls.__name__)
+    cls.logname = "{}.{}".format(cls.__module__, cls.__name__)
     cls._log = logging.getLogger(__name__)
 
     # Set level to debug so filter is done by handler
     cls._log.setLevel(logging.DEBUG)
 
     # Console Handler
-    #ch = logging.StreamHandler(sys.stderr)
-    #ch.set_name("stderr")
-    #ch.setLevel(logging.CRITICAL)
+    ch = logging.StreamHandler(sys.stderr)
+    ch.set_name("stderr")
+    ch.setLevel(logging.CRITICAL)
 
-    #ch2 = logging.StreamHandler(sys.stdout)
-    #ch2.set_name("stdout")
-    #ch2.setLevel(console_level)
+    ch2 = logging.StreamHandler(sys.stdout)
+    ch2.set_name("stdout")
+    ch2.setLevel(console_level)
 
-    #formatter = logging.Formatter("{asctime} - {levelname:<8}| {message}", style="{")
+    formatter = logging.Formatter("{asctime} - {levelname:<8}| {message}", style="{")
 
     # File Handler
-    #_PERMISSION_TO_WRITE = True
-    #logUserPath = expanduser("~")
-    #logSaveFilePath = join(logUserPath, ".BAC0")
+    _PERMISSION_TO_WRITE = True
+    logUserPath = expanduser("~")
+    logSaveFilePath = join(logUserPath, ".BAC0")
 
-    #logFile = join(logSaveFilePath, "BAC0.log")
-    #if not os.path.exists(logSaveFilePath):
-    #    try:
-    #        os.makedirs(logSaveFilePath)
-    #    except:
-    #        _PERMISSION_TO_WRITE = False
-    #if _PERMISSION_TO_WRITE:
-    #    fh = FileHandler(logFile)
-    #    fh.set_name("file_handler")
-    #    fh.setLevel(file_level)
-    #    fh.setFormatter(formatter)
+    logFile = join(logSaveFilePath, "BAC0.log")
+    if not os.path.exists(logSaveFilePath):
+        try:
+            os.makedirs(logSaveFilePath)
+        except:
+            _PERMISSION_TO_WRITE = False
+    if _PERMISSION_TO_WRITE:
+        fh = FileHandler(logFile)
+        fh.set_name("file_handler")
+        fh.setLevel(file_level)
+        fh.setFormatter(formatter)
 
-    #ch.setFormatter(formatter)
-    #ch2.setFormatter(formatter)
+    ch.setFormatter(formatter)
+    ch2.setFormatter(formatter)
     # Add handlers the first time only...
-    #if not len(cls._log.handlers):
-    #    if _PERMISSION_TO_WRITE:
-    #        cls._log.addHandler(fh)
-    #    cls._log.addHandler(ch)
-    #    cls._log.addHandler(ch2)
+    if not len(cls._log.handlers):
+        if _PERMISSION_TO_WRITE:
+            cls._log.addHandler(fh)
+        cls._log.addHandler(ch)
+        cls._log.addHandler(ch2)
 
     LogList.LOGGERS.append(cls._log)
 
